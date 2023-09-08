6c8ed756cbc0d1fbd4b4471718290c4002d96109
sam88mohd <sam88mohd@gmail.com>
2023-09-08 22:34:52 +0800

    Add models and controller

---
+++ Engine/Controller/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Controller
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Abu";
+            CurrentPlayer.Gold = 100000;
+        }
+    }
+}
