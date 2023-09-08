3c81662f7265bdef1c5af2e4870f770ce94f09d2
sam88mohd <sam88mohd@gmail.com>
2023-09-08 23:13:44 +0800

    connect view with controller

--- Engine/Controller/GameSession.cs
+++ Engine/Controller/GameSession.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 
 namespace Engine.Controller
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Abu";
+            CurrentPlayer.Category = "Fighter";
+            CurrentPlayer.ExperiencePoints = 10;
+            CurrentPlayer.HitPoints = 20;
+            CurrentPlayer.Level = 1;
             CurrentPlayer.Gold = 100000;
         }
     }
